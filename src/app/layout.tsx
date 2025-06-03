a85788d2a6134c8b53b589e56e96dcbc773a93bb
tare <siltarre@gmail.com>
2025-06-03 20:44:20 +0900

    ✨ header 컴포넌트 레이아웃

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Header from '@/components/layout/Header';
 
 export const metadata: Metadata = {
   title: 'wakever',
@@ -13,7 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Bell, Search } from 'lucide-react';
+
+export default function Header() {
+  return (
+    <header className="border-b">
+      <div className="container mx-auto flex h-16 items-center justify-between px-5">
+        <h1>
+          <Link href="/">
+            <Image src="/logo.svg" alt="wakever" width={99} height={26} />
+          </Link>
+        </h1>
+        <div className="flex items-center gap-4 text-gray-400">
+          <Search className="size-6" />
+          <Bell className="size-6" />
+        </div>
+      </div>
+    </header>
+  );
+}
